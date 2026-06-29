ae142036e3de6b15c6d3f95d51e9571f7360f1b5
Erwan Le Roux <erwan.le-roux@imt-atlantique.fr>
2026-06-29 16:22:49 +0200

    update main_train_network.py

--- main_train_network.py
+++ main_train_network.py
@@ -1,6 +1,8 @@
 from torch import from_numpy
 
 from datasets.get_dataset_v1 import get_dataset_v1
+from datasets.get_dataset_v2 import get_dataset_v2
+from datasets.get_dataset_v3 import get_dataset_v3
 from neural_net_pytorch_ import NeuralNetwork
 
 
@@ -14,17 +16,19 @@ def train_network(inputs, outputs, epochs: int, lr: float, network_name: str):
     model.train_model(X, y, cost_fn=cost_fn, epochs=epochs, lr=lr)
     model.register_to_csv(f"models/{network_name}.csv")
 
-def main_train_network(version: int = 1, N: int = 100000, epochs: int = 100000, lr: float = 0.001):
+def main_train_network():
     # Dataset settings
-    version = 1
+    N = 100000
+    version = 3
     K = 4
     u_bar = 20
     R = 10
     # Training settings
-    epochs = 100_000
-    lr = 0.001
+    epochs = 10_000
+    lr = 0.005
     # Training of the network
-    inputs, outputs = get_dataset_v1(N, K, u_bar, R)
+    get_dataset = [get_dataset_v1, get_dataset_v2, get_dataset_v3][version + 1]
+    inputs, outputs = get_dataset(N, K, u_bar, R)
     network_name = f'network_v{version}_{N}_{K}_{u_bar}_{R}_{epochs}'
     train_network(inputs, outputs, epochs, lr, network_name)
 
