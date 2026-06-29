6eacc263fa62cca1ac49633b12463d8cbba5e268
Erwan Le Roux <erwan.le-roux@imt-atlantique.fr>
2026-06-29 15:22:24 +0200

    add dataset python files

--- datasets/get_dataset_v2.py
+++ datasets/get_dataset_v2.py
@@ -1,24 +1,12 @@
-import math
-from random import uniform
-
 import numpy as np
 
 
-def get_dataset_v2(N: int, K: float, u_bar: float, R: float) -> tuple[np.ndarray, np.ndarray]:
+def get_dataset_v2(N: int, K: float = 1, u_bar: float = 5, R: float= 10) -> tuple[np.ndarray, np.ndarray]:
     "Sample centred on the circle "
-    inputs = []
-    outputs = []
-    limit = 100
-    for _ in range(N):
-        # ray =
-        theta = uniform(0, 2 * math.pi)
-        inputs.append([x, y, math.cos(theta), math.sin(theta)])
-        u1, u2 = motion_optimal(x, y, theta, K, u_bar, R)
-        outputs.append([u1, u2])
-    return np.array(inputs, dtype=np.float32), np.array(outputs, dtype=np.float32)
+    pass
 
 
 if __name__ == '__main__':
-    inputs, outputs = get_dataset_v1(10, 2, 2, 2)
+    inputs, outputs = get_dataset_v2(10)
     print(inputs.shape, inputs)
     print(outputs.shape, outputs)
--- datasets/get_dataset_v3.py
+++ datasets/get_dataset_v3.py
@@ -1,6 +1,6 @@
 import numpy as np
 
 
-def get_dataset_v3(N: int, K: float, u_bar: float, R: float) -> tuple[np.ndarray, np.ndarray]:
+def get_dataset_v3(N: int, K: float = 1, u_bar: float = 5, R: float= 10) -> tuple[np.ndarray, np.ndarray]:
     "Half of the data sample randomly everywhere, half sample centred on the circle"
     pass
